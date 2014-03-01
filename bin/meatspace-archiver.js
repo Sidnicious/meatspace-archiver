b688c0f1650c119f45b43cb3b339b826367c8a3b
Sidney San Martín <sidney@s4y.us>
2014-03-01 16:59:14 -0500

    Fork! This is now a Meatspace archiver

---
+++ bin/meatspace-archiver.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+'use strict';
+
+var socket = require( 'socket.io-client' );
+
+var URL = 'https://' + process.argv[2] + '.meatspac.es/';
+
+var c = socket.connect( URL ).on( 'connect', function() {
+
+	c.on( 'message', function(message) {
+		process.stdout.write(JSON.stringify(message));
+		process.stdout.write('\n');
+	});
+
+});
